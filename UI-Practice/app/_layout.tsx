7bfc1cca07284c1ade739f759a8cafed6f42b018
muhammad-mubashir-jawed <contact.mubashir.j@gmail.com>
2024-11-28 12:59:46 +0500

    process.....

--- UI-Practice/app/_layout.tsx
+++ UI-Practice/app/_layout.tsx
@@ -1,12 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Text, View, StyleSheet } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +19,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -28,12 +33,29 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View style={Styles.container}>
+      <View style={Styles.header}>
+        <Text style={Styles.headerTxt}>BYKEA</Text>
+      </View>
+    </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerTxt: {
+    fontWeight: "bold",
+    letterSpacing: 3,
+    fontSize: 21,
+  },
+  header: {
+    height: 50,
+    borderBottomColor: "#ccc", 
+    borderBottomWidth: 1, 
+    flexDirection: "row",
+    alignItems: "center", 
+    justifyContent: "center", 
+  },
+});
